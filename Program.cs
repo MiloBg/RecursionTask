463dfad38cc43e8e62e69ae8d0b640cccdfd33e1
MiloBg <milofcrs@gmail.com>
2022-04-19 11:27:46 +0200

    Connection Test

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace RecursionTask
 {
@@ -6,19 +8,32 @@ namespace RecursionTask
     {
         static void Main(string[] args)
         {
-            string txtPath = @"/Data.txt";
-            List<string> lines = new List<string>();
-            List<Lines> textData = new List<Lines>();
+            // string txtPath = @"/Data.txt";
+            // List<string> lines = new List<string>();
+            // List<Lines> textData = new List<Lines>();
 
-            lines = File.ReadAllLines(txtPath).ToList();
+            // lines = File.ReadAllLines(txtPath).ToList();
 
-            foreach (var line in lines)
-            {
-                string[] items = line.Split(',');
-                Lines l = new Lines(int.Parse(items[0]), items[1], int.Parse(items[2]));
-                textData.Add(l);
+            // foreach (var line in lines)
+            // {
+            //     string[] items = line.Split(',');
+            //     Lines l = new Lines(int.Parse(items[0]), items[1], int.Parse(items[2]));
+            //     textData.Add(l);
+            // }
+            // Lines.RecursiveMethod(textData, 0, 0);
+
+            var connectionString = "Data Source=localhost;Initial Catalog=DatabaseName;Integrated Security=True";
+
+            var getLinesQuery = "select * from Lines";
+            using (var connection = new SqlConnection(connectionString)) {
+                var lines = connection.Query<Lines>(getLinesQuery).ToList();
             }
-            Lines.RecursiveMethod(textData, 0, 0);
+
+            var insertLinesQuery = "insert into Lines (Text, ParentID) values (@Text, @ParentID)";
+            using (var connection = new SqlConnection(connectionString)) {
+                connection.Execute(insertLinesQuery, new { Text = "text2", ParentID = 1});
+                var lines = connection.Query<Lines>(getLinesQuery).ToList();
+            }
         }
     }
 }
